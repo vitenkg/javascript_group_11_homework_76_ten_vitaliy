3bf32ef2f85e33f004f332e2beeb5ca660594095
Vitaliy Ten <vitalten10@gmail.com>
2021-10-11 18:46:08 +0600

    back commit

---
+++ backend/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const cors = require('cors');
+const messages = require('./app/messages');
+const modules = require('./modules/ModMessage');
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+const port = 8000;
+
+app.use('/messages', messages);
+
+modules.init();
+app.listen(port, () => {
+  console.log('Server started on ', port);
+});
+
